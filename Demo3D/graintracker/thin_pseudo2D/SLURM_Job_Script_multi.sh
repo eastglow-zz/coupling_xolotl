0192e9c0c4c9208571441b4d815f37e6841682b5
Dong-Uk Kim <donguk.kim@login3.ufhpc>
2019-11-13 06:48:05 -0500

    Input files with GrainTracker in 2D and 3D added

---
+++ Demo3D/graintracker/thin_pseudo2D/SLURM_Job_Script_multi.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --job-name=FG64_P2D            #Job name
+#SBATCH --nodes=4                   #Number of nodes (servers, 32 proc/node)
+#SBATCH --ntasks=64                 #Number of tasks/How many parallel jobs to run
+#SBATCH --ntasks-per-node=16         #Tasks per node
+##SBATCH --cpus-per-task=1           #Number of CPU per task
+#SBATCH --mem-per-cpu=600mb           #Memory (120 gig/node)
+#SBATCH --time=96:00:00             #Walltime hh:mm:ss
+#SBATCH --output=FG2D-%j.out #Output and error log name
+#SBATCH --mail-type=ALL             #When to email user: NONE,BEGIN,END,FAIL,REQUEUE,ALL
+#SBATCH --mail-user=donguk.kim@ufl.edu       #Email address to send mail to
+#SBATCH --qos=michael.tonks                #Allocation group name, add -b for burst job
+##SBATCH --partition=hpg2-compute
+##SBATCH --array=1-200%10           #Used to submit multiple jobs with one submit
+
+APPPATH=~/gcc_moose/projects/coupling_xolotl
+srun --mpi=pmix_v1 $APPPATH/coupling_xolotl-opt -i ./GPM_grain_tracker.i
